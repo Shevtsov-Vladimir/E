751ec91c2ee5548ec9c037bc757fc28c49461072
Shevtsov-Vladimir <93482821+Shevtsov-Vladimir@users.noreply.github.com>
2021-12-12 00:24:32 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
 let menuIcon = document.querySelector('.menu-icon');
 let showMenu = document.querySelector('.menu');
-let body = document.querySelector('.scroll');
 
-menuIcon.addEventListener('click', function() {
+menuIcon.addEventListener('click', function () {
     menuIcon.classList.toggle('active')
     showMenu.classList.toggle('active')
-    body.classList.toggle('active')
+    document.body.classList.toggle('no-scroll')
+    showMenu.scrollTop = 0
 
-    if(!body.classList.contains('active')) {
-        body.classList.toggle('not-active')
-
-    }else {
-        body.classList.remove('not-active')
-    }
-
-    if(!menuIcon.classList.contains('active')) {
+    if (!menuIcon.classList.contains('active')) {
         menuIcon.classList.toggle('not-active')
 
-    }else {
+    } else {
         menuIcon.classList.remove('not-active')
     }
 
-    // if(body.style.overflow === 'hidden') {
-    //     document.body.style.overflow='scroll';
-    // } else {
-    //     document.body.style.overflow='hidden';
-    // }
+    $(window).resize(function () {
+        if (showMenu.classList.contains('active') && (document.documentElement.clientWidth > 768)) {
+            showMenu.classList.remove("active");
+            menuIcon.classList.remove('active')
+            showMenu.classList.toggle('active')
+            document.body.classList.toggle('no-scroll')
 
-    if(!showMenu.classList.contains('active')) {
-        showMenu.classList.toggle('not-active')
-
-    }else {
-        showMenu.classList.remove('not-active')
-    }
-    
+        }
+    });
 });
 
+window.addEventListener('scroll', () => {
+    let scrollDisntance = window.scrollY;
+
+    let listItems = [
+        document.getElementById('home'),
+        document.getElementById('about'),
+        document.getElementById('skills'),
+        document.getElementById('portfolio'),
+        document.getElementById('contacts')];
+
+    listItems.forEach((el, i) => {
+        if (el.OffsetTop = document.querySelector('.menu').clientHeight <= scrollDisntance) {
+            document.querySelectorAll('.menu li').forEach((el) => {
+                if (el.classList.contains('active')) {
+                    el.classList.remove('active');
+                }
+            });
+
+            document.querySelectorAll('.menu li ')[i].querySelector('li').classList.add('active');
+        }
+    });
+});
